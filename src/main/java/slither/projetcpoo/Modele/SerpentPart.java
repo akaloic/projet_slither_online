bf538e5f023bbd32c0b25ac0ef99d94407ca5125
JIRAUD loic <loic.jiraud@etu.u-paris.fr>
2023-11-29 13:40:28 +0100

    maj

---
+++ src/main/java/slither/projetcpoo/Modele/SerpentPart.java
@@ -0,0 +1,31 @@
+package com.example.projetcpoo;
+
+import com.example.projetcpoo.Main;
+
+public class SerpentPart {
+    public static final double SNAKEPARTSIZE = Main.SCREENLENGTH.getWidth() / 100;
+    private double x;
+    private double y;
+
+    public SerpentPart(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    // ajouter setteurs pour modifier la pose du serpent/de la tete
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
---
+++ src/main/java/slither/projetcpoo/View/GameView.java
@@ -0,0 +1,53 @@
+package com.example.projetcpoo;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import com.example.projetcpoo.Modele.Modele;
+
+import javafx.scene.canvas.Canvas;
+
+public class GameView {
+    private GraphicsContext graphicsContext;
+    private Canvas canvas;
+    private Modele modele;
+
+    public GameView(Modele modele, GraphicsContext graphicsContext) {
+        this.graphicsContext = graphicsContext;
+        this.canvas = graphicsContext.getCanvas();
+        this.modele = modele;
+    }
+
+    public void draw() {
+        drawSnake();
+        drawFood();
+    }
+
+    private void drawSnake() {
+        Serpent serpent = modele.getSerpentJoueur();
+        graphicsContext.setFill(serpent.getCouleur());
+        graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        for (int i = 0; i < serpent.getTaille(); i++) {
+            graphicsContext.fillOval(serpent.getSegments().get(i).getX(), serpent.getSegments().get(i).getY(),
+                    SerpentPart.SNAKEPARTSIZE, SerpentPart.SNAKEPARTSIZE);
+        }
+    }
+
+    private void drawFood() {
+        Food[] food = modele.getFood();
+        for (int i = 0; i < food.length; i++) {
+            graphicsContext.setFill(food[i].getCouleur());
+            graphicsContext.fillOval(food[i].getX(), food[i].getY(), Food.FOODSIZE, Food.FOODSIZE);
+        }
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    public void updateModele(Modele modele) {
+        this.modele = modele;
+    }
+
+    // Ajouter d autre methode pour le reste du jeu
+}
