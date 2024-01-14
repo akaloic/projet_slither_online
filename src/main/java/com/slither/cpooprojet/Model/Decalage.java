c4119e5ed7fcdfaadcf443720259fd030204a5c7
JIRAUD loic <loic.jiraud@etu.u-paris.fr>
2024-01-14 12:48:52 +0100

    maj

--- src/main/java/com/slither/cpooprojet/Model/Decalage.java
+++ src/main/java/com/slither/cpooprojet/Model/Decalage.java
@@ -2,12 +2,6 @@ package com.slither.cpooprojet.Model;
 
 import com.slither.cpooprojet.Model.SerializableObject.Rectangle;
 
-// import com.slither.cpooprojet.Model.SerializableObject.Rectangle;
-
-// public sealed interface Decalage permits Food, Snake, Rectangle {
-//     public void decallement(double x, double y);
-// }
-
 public sealed abstract class Decalage permits Food, Snake, Rectangle {
     public abstract void decallement(double x, double y);
 }
